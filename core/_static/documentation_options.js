ef06953c93e6a51bbd86467306daef4244a62c27
aheejin <aheejin@users.noreply.github.com>
2023-01-14 00:57:35 +0000

    deploy: 1782235239ddebaf2cb079b00fdaa2d2c4dedba3

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1 (Draft 2022-03-19)',
-    LANGUAGE: 'None',
+    VERSION: '2.0 (Draft 2023-01-14)',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
